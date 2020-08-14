7212088414bf490bda7c13b72d8f65983f99a630
alexandrerosseto <alexandrerosseto@gmail.com>
2020-08-14 09:19:37 -0300

    Added: Navigation, disable menu on main screeen, categories (backend) listing

---
+++ src/pages/categories/categories.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoryService } from '../../services/domain/category.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-categories',
+  templateUrl: 'categories.html',
+})
+export class CategoriesPage {
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoryService: CategoryService) {
+  }
+
+  ionViewDidLoad() {
+    this.categoryService.findAll()
+    .subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 
@@ -9,8 +9,18 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public menu: MenuController) {
   }
 
+  public ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  
+  public ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  public login() {
+    this.navCtrl.setRoot("CategoriesPage");
+  }
 }
